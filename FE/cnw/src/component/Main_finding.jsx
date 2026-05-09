496b5fab5b35437757059938139647ba13a8bf95
hieuhoang117 <hieuhoangminh2403@gmail.com>
2026-05-09 23:12:43 +0700

    finding actor

--- FE/cnw/src/component/Main_finding.jsx
+++ FE/cnw/src/component/Main_finding.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import "./Main_finding.css";
 import MovieItem from "./Movie_Iterm";
+import ActorItem from "./actorIterm";
 import { useParams } from "react-router-dom";
 
 const Main_finding = () => {
     const [movie, setmovie] = useState([]);
     const { searchInput } = useParams();
+    const [actors, setActors] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -22,10 +24,33 @@ const Main_finding = () => {
             fetchData();
         }
     }, [searchInput]);
+    useEffect(() => {
+        const handleFindActor = async (name) => {
+            try {
+                const res = await fetch(`http://localhost:5000/api/actor/name/${name}`);
+                const data = await res.json();
+                setActors(data);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        if (searchInput) {
+            handleFindActor(searchInput);
+        }
+
+    } , [searchInput]);
 
     return (
         <div className="main-finding">
             <h1 className="finding-title">Kết quả tìm kiếm theo: "{searchInput}"</h1>
+            <div className="movie-list_actor">
+                {actors.map((actor) => (
+                    <div className="actor-card-wrapper" key={actor.IDactor}>
+                        <ActorItem actor={actor} />
+                    </div>
+                ))}
+            </div>
             <div className="movie-list_genre">
                 {movie.map((watchedItem) => (
                     <div className="movie-card-wrapper" key={watchedItem.IDmovie}>
---
+++ FE/cnw/src/component/actorIterm.jsx
@@ -0,0 +1,19 @@
+import "./actorIterm.css";
+import { useNavigate } from "react-router-dom";
+
+const ActorItem = ({ actor }) => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="actor-item" onClick={() => navigate(`/actor/${actor.IDactor}`)}>
+            <img 
+                src={actor.Photo || "/default-actor.png"} 
+                alt={actor.ActorName} 
+                onError={(e) => e.target.src = "/default-actor.png"}
+            />
+            <h3>{actor.ActorName}</h3>
+        </div>
+    );
+};
+
+export default ActorItem;
